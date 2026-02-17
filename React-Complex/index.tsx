f9360dd2946e53c47badfa9f106ef99fcbeddd3a
Nick <32971513+Psychosynthesis@users.noreply.github.com>
2026-02-17 17:35:21 +0300

    Add comments for application context
    Added comments for consideration on real application experience.

--- React-Complex/index.tsx
+++ React-Complex/index.tsx
@@ -3,6 +3,8 @@ import { Loading, Badge } from '@components';
 import { Profile } from './profile';
 
 export const App = () => {
+  // Возможно тут также следует учесть что-либо
+  // Сделайте предположение на основе вашего опыта работы с реальными приложениями
   return (
     <Profile username="john_doe">
       {(user) => (user === null ? <Loading /> : <Badge info={user} />)}
